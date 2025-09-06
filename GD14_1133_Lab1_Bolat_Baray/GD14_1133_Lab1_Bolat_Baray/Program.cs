27b026b6a88031b37e9f3c5416606951dbbf76a5
gamblin0 <baraybolat@gmail.com>
2025-09-05 20:44:41 -0700

    moving projects to repo
    added first week project to repository

---
+++ GD14_1133_Lab1_Bolat_Baray/GD14_1133_Lab1_Bolat_Baray/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD14_1133_Lab1_Bolat_Baray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yogurt");
+            Console.WriteLine("Gurt:yo");
+        }
+    }
+}
