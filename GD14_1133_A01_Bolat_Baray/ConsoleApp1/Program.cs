91cdacba3b7183dc42a843e1994017572f028a62
gamblin0 <baraybolat@gmail.com>
2025-09-08 16:51:27 -0700

    added goodbye part
    Forgot to put the goodbye message on first submission so I added it on this one

--- GD14_1133_A01_Bolat_Baray/ConsoleApp1/Program.cs
+++ GD14_1133_A01_Bolat_Baray/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Decimal: 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
             Console.WriteLine("Binary: 0, 1, 10, 100, 1000, 10000, 100000, 1000000,1100100, 11111111");
             Console.WriteLine("Hex: 00, 01, 02, 04, 08, 10, 20, 40, 64, FF");
-            Console.WriteLine("Message: Hello, I'm Baray Bolat from GD14 and today's date is 2025-09-08.");
+            Console.WriteLine("Message: Hello, I'm Baray Bolat from GD14 and today's date is 2025-09.This was fun! Goodbye.");
         }
     }
 }
