39dba6c3745cce6a7289504059f141150209b47c
gamblin0 <baraybolat@gmail.com>
2025-09-10 14:06:49 -0700

    added goodbye message
    added goodbye message

--- GD14_1133_A01_Bolat_Baray/1133_L2_BarayBolat_001/Program.cs
+++ GD14_1133_A01_Bolat_Baray/1133_L2_BarayBolat_001/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace _1133_L2_BarayBolat_001
+﻿using System;
+
+namespace _1133_L2_BarayBolat_001
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Message: Hello, I'm Baray Bolat from GD14 and today's date is 2025-09-08.");
             Console.WriteLine("Decimal: 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
             Console.WriteLine("Binary: 0, 1, 10, 100, 1000, 10000, 100000, 1000000,1100100, 11111111");
             Console.WriteLine("Hex: 00, 01, 02, 04, 08, 10, 20, 40, 64, FF");
-            Console.WriteLine("Message: Hello, I'm Baray Bolat from GD14 and today's date is 2025-09-08.");
+            Console.WriteLine("Message: Hello, I did some changes and now the date is 2025-09-10, goodbye");
         }
     }
 }
