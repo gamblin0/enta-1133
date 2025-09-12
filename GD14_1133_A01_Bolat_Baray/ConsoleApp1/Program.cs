643d8182fb901bccf80cca355217d939e9cbdd41
gamblin0 <baraybolat@gmail.com>
2025-09-12 16:34:39 -0700

    organizing
    I orginazed the output and made it more understandable

--- GD14_1133_A01_Bolat_Baray/ConsoleApp1/Program.cs
+++ GD14_1133_A01_Bolat_Baray/ConsoleApp1/Program.cs
@@ -7,11 +7,20 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Message: Hello, I'm Baray Bolat from GD14 and today's date is 2025-09-08.");
-            Console.WriteLine("Decimal | Binary | Hex ");
-            Console.WriteLine("Decimal: 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
-            Console.WriteLine("Binary: 0, 1, 10, 100, 1000, 10000, 100000, 1000000,1100100, 11111111");
-            Console.WriteLine("Hex: 00, 01, 02, 04, 08, 10, 20, 40, 64, FF");
+            Console.WriteLine(" Decimal  |  Binary  |  Hex ");
+            Console.WriteLine("----------|----------|------");
+            Console.WriteLine("    0     |     0    |   00 ");
+            Console.WriteLine("    1     |     1    |   01 ");
+            Console.WriteLine("    2     |    10    |   02 ");
+            Console.WriteLine("    4     |    100   |   04 ");
+            Console.WriteLine("    8     |   1000   |   08 ");
+            Console.WriteLine("   16     |   10000  |   10 ");
+            Console.WriteLine("   32     |  100000  |   20 ");
+            Console.WriteLine("   64     |  1000000 |   40 ");
+            Console.WriteLine("   100    |  1100100 |   64 ");
+            Console.WriteLine("   255    | 11111111 |   FF ");
             Console.WriteLine("Message: Hello, I did some changes and now the date is 2025-09-10, goodbye.");
+            Console.WriteLine("Message: Hello again, organized the output and now today's date is 2025-09-12.");
         }
     }
 }
