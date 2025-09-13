b009bf329847beb6b16b3b53cf30770cbdcc8a0d
gamblin0 <baraybolat@gmail.com>
2025-09-12 21:49:14 -0700

    creation of lab3
    created needed docs for lab3

---
+++ Baray_Bolat_DiceGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Baray_Bolat_DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Baray_Bolat_DiceGame/Scripts/GameMenager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baray_Bolat_DiceGame.Scripts
+{
+    internal class GameMenager
+    {
+        private int  
+    }
+}
