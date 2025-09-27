1a4e011ccb4cf262dc5075195b16c1b61fc07f0c
gamblin0 <baraybolat@gmail.com>
2025-09-26 17:56:44 -0700

    fixed Program.cs back
    changed internal void to static void

--- Baray_Bolat_DiceGame/Program.cs
+++ Baray_Bolat_DiceGame/Program.cs
@@ -1,14 +1,13 @@
 ﻿using Baray_Bolat_DiceGame.Scripts;
-using static Baray_Bolat_DiceGame.Scripts.Die;
 
 namespace Baray_Bolat_DiceGame
 {
     internal class Program
     {
-        internal void Main(string[] args)
+        static void Main(string[] args)
         {
             //created an instance to call the gamemenager to program
-            GameMenager Menager = new GameMenager(); //constuctor
+           GameMenager Menager = new GameMenager(); //constuctor
             Menager.Playgame();
         }
     }
