3c184679d8360f119783c69da52ca6d313032a92
gamblin0 <baraybolat@gmail.com>
2025-10-08 16:17:31 -0700

    setups
    setups for the new assignment

---
+++ Baray_Bolat_DiceGame/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baray_Bolat_DiceGame.Scripts
+{
+    internal class MapGenerator
+    {
+        int[,] Dungeon =
+        {
+            { 1 ,2 ,3},
+            { 4 ,5 ,6},
+            { 7 ,8 ,9}
+
+        };
+    }
+}
---
+++ Baray_Bolat_DiceGame/Scripts/Rooms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baray_Bolat_DiceGame.Scripts
+{
+    internal class Rooms
+    {
+        public void RoomDescription()
+        {
+
+        }
+
+        public void OnRoomEntered()
+        {
+
+        }
+
+        public void OnRoomSearched()
+        {
+
+        }
+
+        public void OnRoomExit()
+        {
+
+        }
+
+
+
+    }
+}
