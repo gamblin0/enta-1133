6ec149e45822e10c38d16c519624d105197a260a
gamblin0 <baraybolat@gmail.com>
2025-10-15 18:22:18 -0700

    setting up for assignment 2
    setting up for classes assignment 2

---
+++ Baray_Bolat_DiceGame/Scripts/Items.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baray_Bolat_DiceGame.Scripts
+{
+    internal abstract class Items
+    {
+        public Items()
+        {
+
+        }
+
+        internal interface IWeapon { }
+
+        internal interface IConsumable { }
+
+        internal interface ITool { }
+
+        internal interface ILoot { }
+
+        public class Dagger : Items, IWeapon
+        {
+
+        }
+
+        public class Hatchet : Items, IWeapon
+        {
+
+        }
+
+        public class Axe : Items, IWeapon
+        {
+
+        }
+
+        public class Baretta : Items, IWeapon
+        {
+
+        }
+
+        public class GOLD : Items, ILoot
+        {
+
+        }
+
+        public class DonerKebap : Items, ILoot
+        {
+
+        }
+    }
+}
